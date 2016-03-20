29f69d82325f6188a94a00eaf0a5d55d4ca24548
Michael Gissler <michael.gissler@gmail.com>
2016-03-20 16:37:38 +0100

    completed lecture 12 - multiple components

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,15 @@
 import {Component} from 'angular2/core';
+import {MyComponentComponent} from './my-component.component';
+import {MySecondComponent} from './my-second-component.component';
 
 @Component({
     selector: 'app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <h2>Angular 2 Udemy Course</h2>       
+        <my-component>loading...</my-component>
+        <my-second-component>loading...</my-second-component>
     `,
+    directives: [MyComponentComponent, MySecondComponent]
 })
 export class AppComponent {
 
---
+++ dev/my-second-component.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-second-component',
+    template: `
+    <h3>this is my second component</h3>
+    `
+})
+export class MySecondComponent {
+    
+}
