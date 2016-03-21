161d9fd84493715a2e80cb88dd16048e4f9f7794
Michael Gissler <michaelgissler@BG-MB-AIR-97.local>
2016-03-21 08:33:42 +0100

    moved app template into a single template html file

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -4,13 +4,7 @@ import {MySecondComponent} from './my-second-component.component';
 
 @Component({
     selector: 'app',
-    template: `
-        <h2>Angular 2 Udemy Course</h2>  
-        <h3>hello, {{name}}. Do you feel good? <span [class.is-awesome] = "inputElement.value === 'yes' ">I'm feeling good!</span> </h3> 
-        <input type="text" #inputElement (keyup) = "0" >            
-        <my-component>loading...</my-component>
-        <my-second-component>loading...</my-second-component>
-    `,
+    templateUrl: './app/app.template.html',
     styleUrls: ['src/css/myComponent.css'],
     directives: [MyComponentComponent, MySecondComponent]
 })
