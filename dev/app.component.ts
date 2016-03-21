3ec500425ab555c07a3c2cabb240230ac001a48f
Michael Gissler <michaelgissler@BG-MB-AIR-97.local>
2016-03-21 08:27:01 +0100

    completed lecture 13 - manipulating templates

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -5,12 +5,15 @@ import {MySecondComponent} from './my-second-component.component';
 @Component({
     selector: 'app',
     template: `
-        <h2>Angular 2 Udemy Course</h2>       
+        <h2>Angular 2 Udemy Course</h2>  
+        <h3>hello, {{name}}. Do you feel good? <span [class.is-awesome] = "inputElement.value === 'yes' ">I'm feeling good!</span> </h3> 
+        <input type="text" #inputElement (keyup) = "0" >            
         <my-component>loading...</my-component>
         <my-second-component>loading...</my-second-component>
     `,
+    styleUrls: ['src/css/myComponent.css'],
     directives: [MyComponentComponent, MySecondComponent]
 })
 export class AppComponent {
-
+    name = "Michael";
 }
