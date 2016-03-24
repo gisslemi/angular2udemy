38d90cb2384131393e7f0dbd752efeedfc165640
Michael Gissler <michaelgissler@BG-MB-AIR-97.local>
2016-03-24 19:34:52 +0100

    initial commit, based on boilerplate

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,8 @@
 import {Component} from 'angular2/core';
-import {MyComponentComponent} from './my-component.component';
-import {MySecondComponent} from './my-second-component.component';
 
 @Component({
     selector: 'app',
-    templateUrl: './app/app.template.html',    
-    directives: [MyComponentComponent, MySecondComponent]
+    templateUrl: './app/app.template.html'    
 })
 export class AppComponent {  
 }
