39a27f59303ba7cfce7cb75a5b942d92cedee445
Michael Gissler <michaelgissler@BG-MB-AIR-97.fritz.box>
2016-03-25 11:05:37 +0100

    completed lecture 17 - puzzle part 1

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,8 +1,10 @@
 import {Component} from 'angular2/core';
+import {PuzzleComponent} from './puzzle/puzzle.component';
 
 @Component({
     selector: 'app',
-    templateUrl: './app/app.template.html'    
+    templateUrl: './app/app.template.html',
+    directives: [PuzzleComponent]       
 })
 export class AppComponent {  
 }
---
+++ dev/puzzle/puzzle.component.ts
@@ -0,0 +1,10 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-puzzle',
+    templateUrl: 'app/puzzle/puzzle.template.html',
+
+})
+export class PuzzleComponent {
+    
+}
