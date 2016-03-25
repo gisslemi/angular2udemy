719595d0a6f6985d090035a4600ffbe0aeb27cb8
Michael Gissler <michaelgissler@BG-MB-AIR-97.fritz.box>
2016-03-25 17:14:05 +0100

    completed lecture 19 - puzzle part 3

--- dev/puzzle/puzzle.component.ts
+++ dev/puzzle/puzzle.component.ts
@@ -1,10 +1,26 @@
 import {Component} from 'angular2/core';
+import {OnInit} from 'angular2/core';
 
 @Component({
     selector: 'my-puzzle',
     templateUrl: 'app/puzzle/puzzle.template.html',
 
 })
-export class PuzzleComponent {
+export class PuzzleComponent implements OnInit {
+    switch1Number: number;
+    switch2Number: number;
+    switch3Number: number;
+    switch4Number: number;
     
+    ngOnInit():any{
+        this.switch1Number = Math.round (Math.random());
+        this.switch2Number = Math.round (Math.random());
+        this.switch3Number = Math.round (Math.random());
+        this.switch4Number = Math.round (Math.random());
+        
+        console.log(this.switch1Number);
+        console.log(this.switch2Number);
+        console.log(this.switch3Number);
+        console.log(this.switch4Number);
+    }
 }
